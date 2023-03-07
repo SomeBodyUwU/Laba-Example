0a66e1ff184b2add01f0ff9cea06792b66014054
SomeBodyUwU <127203403+SomeBodyUwU@users.noreply.github.com>
2023-03-07 13:57:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Text.Json;
+
+var Numbers = new List<int>{1, -1, 4, 20, 3, -9, 5, -8};
+var sortedNumbers = Numbers.Where(n => n < 0).Concat(Numbers.Where(n => n >= 0));
+var json = JsonSerializer.Serialize(sortedNumbers);
+Console.WriteLine(json);
